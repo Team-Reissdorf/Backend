f244537cd305bc7728d57a13a52be6171353e9ac
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2024-12-20 14:06:07 +0100

    Added the swagger documentation for the login post endpoint and defined the LoginResponse

---
+++ endpoints/login.go
@@ -0,0 +1,52 @@
+package endpoints
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+type LoginResponse struct {
+	Token string `json:"token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI8dXNlci1pZD4iLCJuYW1lIjoiPHRva2VuLXR5cGU-IiwiaWF0IjoxNzM0Njk4NzEwfQ.hzvbcP77EO8dnEyy5i-OgoOp8MYYwslfwKx32ZKgrH8"`
+}
+
+// Login handles the user login process.
+// @Summary Login to an existing user account
+// @Description Logs the user into the system with the provided credentials and returns a refresh-JWT.
+// @Tags User Management
+// @Accept json
+// @Produce json
+// @Param User body UserBody true "Email address and password of the user"
+// @Success 200 {object} LoginResponse "Login successful"
+// @Failure 400 {object} ErrorResponse "Invalid request body"
+// @Failure 404 {object} ErrorResponse "User not found"
+// @Failure 500 {object} ErrorResponse "Internal server error"
+// @Router /v1/user/login [post]
+func Login(c *gin.Context) {
+	ctx, span := tracer.Start(c.Request.Context(), "LoginUser")
+	defer span.End()
+
+	// Bind JSON body to struct
+	var body UserBody
+	if err := c.ShouldBindJSON(&body); err != nil {
+		err = errors.Wrap(err, "Failed to bind JSON body")
+		logger.Debug(ctx, err)
+		c.JSON(
+			http.StatusBadRequest,
+			ErrorResponse{
+				Error: "Invalid request body",
+			},
+		)
+		return
+	}
+
+	// ToDo: Implement the login process
+	refreshJWT := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI8dXNlci1pZD4iLCJuYW1lIjoiPHRva2VuLXR5cGU-IiwiaWF0IjoxNzM0Njk4NzEwfQ.hzvbcP77EO8dnEyy5i-OgoOp8MYYwslfwKx32ZKgrH8"
+
+	c.JSON(
+		http.StatusOK,
+		LoginResponse{
+			Token: refreshJWT,
+		},
+	)
+}
