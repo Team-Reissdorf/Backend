bac25539ebb20d9a37f4da0920a5a18e6fb29521
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2024-12-20 11:17:12 +0100

    🌱 Initial commit of the basic project structure

---
+++ endpoints/ping.go
@@ -0,0 +1,55 @@
+package endpoints
+
+import (
+	"github.com/LucaSchmitz2003/FlowWatch"
+	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel"
+	"net/http"
+)
+
+var (
+	tracer = otel.Tracer("PingTracer")
+	logger = FlowWatch.GetLogHelper()
+)
+
+// Ping is a simple ping-pong endpoint to check if the server is running properly.
+// @Summary      Returns a pong response
+// @Description  Simple ping-pong endpoint to check if the server is running properly.
+// @Tags         HealthCheck
+// @Produce      json
+// @Success 200  {object} SuccessResponse "Pong response"
+// @Router       /v1/ping [get]
+func Ping(c *gin.Context) {
+	_, span := tracer.Start(c.Request.Context(), "Ping")
+	defer span.End()
+
+	// Create a response
+	response := SuccessResponse{
+		Message: "pong",
+	}
+
+	logger.Warn(c.Request.Context(), "Pong response sent")
+
+	c.JSON(http.StatusOK, response)
+}
+
+// Teapot is an endpoint that returns a 418 I'm a teapot response.
+// @Summary      Returns a 418 I'm a teapot response
+// @Description  Simple endpoint to return a 418 I'm a teapot response.
+// @Tags         HealthCheck
+// @Produce      json
+// @Router       /v1/coffee [get]
+// @Success      418 {object} ErrorResponse "Error: I'm a teapot"
+func Teapot(c *gin.Context) {
+	_, span := tracer.Start(c.Request.Context(), "Teapot")
+	defer span.End()
+
+	// Create a response
+	response := ErrorResponse{
+		Error: "I'm a teapot. I brew tea, not coffee. ☕🫖",
+	}
+
+	logger.Warn(c.Request.Context(), "Teapot response sent")
+
+	c.JSON(http.StatusTeapot, response)
+}
