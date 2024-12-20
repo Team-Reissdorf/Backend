79f9621e135ff7fad6aa932d1a6cbf40535a307b
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2024-12-20 14:05:08 +0100

    Abstracted user request body to enable usage from login endpoint

--- endpoints/register.go
+++ endpoints/register.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 )
 
-type UserRegisterBody struct {
+type UserBody struct {
 	Email    string `json:"email" binding:"required,email" example:"bob.alice@example.com"`
-	Password string `json:"password" binding:"required" example:"<safe-password>"`
+	Password string `json:"password" binding:"required" example:"<password>"`
 }
 
 // Register handles the user registration process.
@@ -17,18 +17,18 @@ type UserRegisterBody struct {
 // @Tags User Management
 // @Accept json
 // @Produce json
-// @Param Email body UserRegisterBody true "Email-Address of the user"
-// @Success 200 {object} endpoints.SuccessResponse "Registration successful"
-// @Failure 400 {object} endpoints.ErrorResponse "Invalid request body"
-// @Failure 409 {object} endpoints.ErrorResponse "User already exists"
-// @Failure 500 {object} endpoints.ErrorResponse "Internal server error"
+// @Param User body UserBody true "Email address and password of the user"
+// @Success 200 {object} SuccessResponse "Registration successful"
+// @Failure 400 {object} ErrorResponse "Invalid request body"
+// @Failure 409 {object} ErrorResponse "User already exists"
+// @Failure 500 {object} ErrorResponse "Internal server error"
 // @Router /v1/user/register [post]
 func Register(c *gin.Context) {
 	ctx, span := tracer.Start(c.Request.Context(), "RegisterUser")
 	defer span.End()
 
 	// Bind JSON body to struct
-	var body UserRegisterBody
+	var body UserBody
 	if err := c.ShouldBindJSON(&body); err != nil {
 		err = errors.Wrap(err, "Failed to bind JSON body")
 		logger.Debug(ctx, err)
