9610b067d1425cdbc6e03830373b5f9e9dc151a3
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2024-12-26 22:05:32 +0100

    Added hint for admin login

--- endpoints/authMiddleware/isUserActiveCheck.go
+++ endpoints/authMiddleware/isUserActiveCheck.go
@@ -6,7 +6,7 @@ func isUserActive(ctx context.Context, userId string) (bool, error) {
 	ctx, span := tracer.Start(ctx, "isUserActive")
 	defer span.End()
 
-	// ToDo: Implement the function to check if the user exists and the status is active
+	// ToDo: Implement the function to check if the user exists and the status is active (pay attention to the admin user for settings endpoints)
 
 	return true, nil
 }
