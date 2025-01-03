88c5d9721245c0476278f028135ff2c9cea8071a
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2025-01-03 13:54:30 +0100

    Added post endpoint to create an athlete

---
+++ endpoints/createAthlete.go
@@ -0,0 +1,100 @@
+package endpoints
+
+import (
+	"context"
+	"github.com/Team-Reissdorf/Backend/endpoints/standardJsonAnswers"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+var AthleteAlreadyExistsError = errors.New("Athlete already exists")
+
+type AthleteBody struct {
+	FirstName string `json:"firstName" example:"Bob"`
+	LastName  string `json:"lastName" example:"Alice"`
+	Email     string `json:"email" example:"bob.alice@example.com"`
+	BirthDate string `json:"birthDate" example:"DD.MM.YYYY"`
+	Sex       string `json:"sex" example:"<m|w|d>"`
+}
+
+// CreateAthlete creates a new athlete profile
+// @Summary Creates a new athlete profile
+// @Description Creates a new athlete profile with the given data. Duplicate email and birthdate combinations are not allowed.
+// @Tags Athlete Management
+// @Accept json
+// @Produce json
+// @Param Athlete body AthleteBody true "Details of an athlete to create a profile"
+// @Param Authorization  header  string  false  "Access JWT is sent in the Authorization header or set as a http-only cookie"
+// @Success 200 {object} standardJsonAnswers.SuccessResponse "Creation successful"
+// @Failure 400 {object} standardJsonAnswers.ErrorResponse "Invalid request body"
+// @Failure 401 {object} standardJsonAnswers.ErrorResponse "The token is invalid"
+// @Failure 409 {object} standardJsonAnswers.ErrorResponse "Athlete already exists"
+// @Failure 500 {object} standardJsonAnswers.ErrorResponse "Internal server error"
+// @Router /v1/athlete/create [post]
+func CreateAthlete(c *gin.Context) {
+	ctx, span := tracer.Start(c.Request.Context(), "CreateAthlete")
+	defer span.End()
+
+	// Bind JSON body to struct
+	var body AthleteBody
+	if err := c.ShouldBindJSON(&body); err != nil {
+		err = errors.Wrap(err, "Failed to bind JSON body")
+		logger.Debug(ctx, err)
+		c.JSON(
+			http.StatusBadRequest,
+			standardJsonAnswers.ErrorResponse{
+				Error: "Invalid request body",
+			},
+		)
+		c.Abort()
+		return
+	}
+
+	// Get the user id from the context
+	// userId := authHelper.GetUserIdFromContext(ctx, c)
+	// ToDo: Verify that the user has the coach role
+
+	// Create the athlete
+	err1 := createNewAthlete(ctx, body)
+	if errors.Is(err1, AthleteAlreadyExistsError) {
+		logger.Debug(ctx, err1)
+		c.JSON(
+			http.StatusConflict,
+			standardJsonAnswers.ErrorResponse{
+				Error: "Athlete already exists",
+			},
+		)
+		c.Abort()
+		return
+	} else if err1 != nil {
+		err1 = errors.Wrap(err1, "Failed to create athlete")
+		logger.Error(ctx, err1)
+		c.JSON(
+			http.StatusInternalServerError,
+			standardJsonAnswers.ErrorResponse{
+				Error: "Internal server error",
+			},
+		)
+		c.Abort()
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		standardJsonAnswers.SuccessResponse{
+			Message: "Creation successful",
+		},
+	)
+}
+
+func createNewAthlete(ctx context.Context, athlete AthleteBody) error {
+	// ToDo: Check if the email and birthdate combination already exists
+	if true {
+		return AthleteAlreadyExistsError
+	}
+
+	// ToDo: Write the athlete to the database
+
+	return nil
+}
