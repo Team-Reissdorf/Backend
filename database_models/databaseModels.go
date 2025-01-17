9b23ad6eda075292f849490f42f7c93cf635570f
Luca Schmitz <lschmitz@telekom.de>
2025-01-17 14:53:41 +0100

    Improved test database models

--- database_models/databaseModels.go
+++ database_models/databaseModels.go
@@ -1,9 +1,23 @@
 package database_models
 
-type Athlete struct {
+import "gorm.io/gorm"
+
+type Person struct {
+	gorm.Model
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
+}
+
+type Athlete struct {
+	PersonID  uint   `json:"person_id" gorm:"primaryKey"` // FK to person, also PK because athlete is a specialization of person
+	Person    Person `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	BirthDate string `json:"birth_date"`
 	Sex       string `json:"sex"`
+}
+
+type Trainer struct {
+	PersonID uint   `json:"person_id" gorm:"primaryKey"` // FK to person, also PK because trainer is a specialization of person
+	Person   Person `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Password string `json:"password"`
 }
