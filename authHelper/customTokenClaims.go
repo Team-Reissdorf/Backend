494f43ff58539d4a3b3e71e7b21d39dd7ea8e553
Luca Schmitz <lschmitz@telekom.de>
2025-01-29 10:17:54 +0100

    Added the RememberMe value to the token claims

--- authHelper/customTokenClaims.go
+++ authHelper/customTokenClaims.go
@@ -5,5 +5,6 @@ import "github.com/golang-jwt/jwt/v5"
 // CustomClaims is the struct to define the claim structure used by the JWT tokens
 type CustomClaims struct {
 	jwt.RegisteredClaims
-	Name string `json:"name"`
+	Name       string `json:"name"`
+	RememberMe bool   `json:"remember_me"`
 }
