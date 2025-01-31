58d205654bfa31444701a54149e5ae2f28be2f1d
Luca Schmitz <lschmitz@telekom.de>
2025-01-31 16:42:44 +0100

    Fixed example values

--- endpoints/performanceManagement/performanceBody.go
+++ endpoints/performanceManagement/performanceBody.go
@@ -1,7 +1,7 @@
 package performanceManagement
 
 type PerformanceBody struct {
-	Points     uint64 `json:"points" example:"Bob"`
+	Points     uint64 `json:"points" example:"1"`
 	Date       string `json:"date" example:"YYYY-MM-DD"`
 	ExerciseId uint   `json:"exercise_id" example:"1"`
 	AthleteId  uint   `json:"athlete_id" example:"1"`
@@ -9,7 +9,7 @@ type PerformanceBody struct {
 
 type PerformanceBodyWithId struct {
 	PerformanceId uint   `json:"performance_id" example:"1"`
-	Points        uint64 `json:"points" example:"Bob"`
+	Points        uint64 `json:"points" example:"1"`
 	Date          string `json:"date" example:"YYYY-MM-DD"`
 	ExerciseId    uint   `json:"exercise_id" example:"1"`
 	AthleteId     uint   `json:"athlete_id" example:"1"`
