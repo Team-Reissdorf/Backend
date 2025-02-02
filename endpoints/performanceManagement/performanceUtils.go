a0cd484578146b1aad44bae01dd4b1710d8cb6db
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2025-02-02 20:22:04 +0100

    Removed unused function

--- endpoints/performanceManagement/performanceUtils.go
+++ endpoints/performanceManagement/performanceUtils.go
@@ -9,24 +9,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// CheckIfExerciseExists checks if an exercise with the given id exists
-func CheckIfExerciseExists(ctx context.Context, exerciseId uint) (bool, error) {
-	ctx, span := endpoints.Tracer.Start(ctx, "ExerciseExistsCheck")
-	defer span.End()
-
-	var exerciseCount int64
-	err1 := DatabaseFlow.TransactionHandler(ctx, func(tx *gorm.DB) error {
-		err := tx.Model(&databaseUtils.Exercise{}).Where("id = ?", exerciseId).Count(&exerciseCount).Error
-		return err
-	})
-	if err1 != nil {
-		err1 = errors.Wrap(err1, "Failed to check if the exercise exists")
-		return false, err1
-	}
-
-	return exerciseCount > 0, nil
-}
-
 // CreateNewPerformances creates new performances in the database
 func CreateNewPerformances(ctx context.Context, performanceEntries []databaseUtils.Performance) error {
 	ctx, span := endpoints.Tracer.Start(ctx, "createNewPerformances")
