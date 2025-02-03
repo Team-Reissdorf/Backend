2c231f0d315b74c2d13a4e1338caa63723d9f3d7
Luca Schmitz <lschmitz@telekom.de>
2025-02-03 09:17:26 +0100

    Remove unused function

--- endpoints/performanceManagement/performanceUtils.go
+++ endpoints/performanceManagement/performanceUtils.go
@@ -9,26 +9,8 @@ import (
 	"gorm.io/gorm"
 )
 
-// CheckIfExerciseExists checks if an exercise with the given id exists
-func CheckIfExerciseExists(ctx context.Context, exerciseId uint) (bool, error) {
-	ctx, span := endpoints.Tracer.Start(ctx, "ExerciseExistsCheck")
-	defer span.End()
-
-	var exerciseCount int64
-	err1 := DatabaseFlow.TransactionHandler(ctx, func(tx *gorm.DB) error {
-		err := tx.Model(&databaseUtils.Exercise{}).Where("id = ?", exerciseId).Count(&exerciseCount).Error
-		return err
-	})
-	if err1 != nil {
-		err1 = errors.Wrap(err1, "Failed to check if the exercise exists")
-		return false, err1
-	}
-
-	return exerciseCount > 0, nil
-}
-
-// CreateNewPerformances creates new performances in the database
-func CreateNewPerformances(ctx context.Context, performanceEntries []databaseUtils.Performance) error {
+// createNewPerformances creates new performances in the database
+func createNewPerformances(ctx context.Context, performanceEntries []databaseUtils.Performance) error {
 	ctx, span := endpoints.Tracer.Start(ctx, "createNewPerformances")
 	defer span.End()
 
@@ -45,9 +27,9 @@ func CreateNewPerformances(ctx context.Context, performanceEntries []databaseUti
 	return nil
 }
 
-// TranslatePerformanceBody translates the performance body to a performance db entry
-func TranslatePerformanceBody(ctx context.Context, performanceBodies []PerformanceBody) []databaseUtils.Performance {
-	ctx, span := endpoints.Tracer.Start(ctx, "TranslatePerformanceBody")
+// translatePerformanceBody translates the performance body to a performance db entry
+func translatePerformanceBody(ctx context.Context, performanceBodies []PerformanceBody) []databaseUtils.Performance {
+	ctx, span := endpoints.Tracer.Start(ctx, "translatePerformanceBody")
 	defer span.End()
 
 	performances := make([]databaseUtils.Performance, len(performanceBodies))
