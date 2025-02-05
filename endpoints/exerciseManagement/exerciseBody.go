9ddb4f0dfb5359efbac4a65b8ab5226d281c8a2b
Luca Schmitz <lschmitz@telekom.de>
2025-02-05 09:58:56 +0100

    Added the age specifics field

--- endpoints/exerciseManagement/exerciseBody.go
+++ endpoints/exerciseManagement/exerciseBody.go
@@ -5,4 +5,5 @@ type ExerciseBodyWithId struct {
 	Name           string `json:"name" example:"Exercise"`
 	Unit           string `json:"unit" example:"minutes"`
 	DisciplineName string `json:"discipline_name" example:"Discipline"`
+	AgeSpecifics   string `json:"age_specifics" example:"Age specific description"`
 }
