950c52df9e492b89e9744c9613ecd1fa66c4d7a3
Luca Schmitz <lschmitz@telekom.de>
2025-03-27 14:08:21 +0100

    Added unit hints for the value fields

--- databaseUtils/databaseModels.go
+++ databaseUtils/databaseModels.go
@@ -88,9 +88,9 @@ type ExerciseGoal struct {
 	FromAge     uint   `json:"from_age" gorm:"uniqueIndex:unique_combination_exercise_goals"`
 	ToAge       uint   `json:"to_age" gorm:"uniqueIndex:unique_combination_exercise_goals"`
 	Sex         string `json:"sex" gorm:"uniqueIndex:unique_combination_exercise_goals;type:char(1);not null"`
-	Bronze      uint64 `json:"bronze"`
-	Silver      uint64 `json:"silver"`
-	Gold        uint64 `json:"gold"`
+	Bronze      uint64 `json:"bronze"` // Time: ms; Distance: cm; Points; Bool: <0|1>
+	Silver      uint64 `json:"silver"` // Time: ms; Distance: cm; Points; Bool: <0|1>
+	Gold        uint64 `json:"gold"`   // Time: ms; Distance: cm; Points; Bool: <0|1>
 	Description string `json:"description"`
 }
 
