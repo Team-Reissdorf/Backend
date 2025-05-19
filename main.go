ae883b681d62d7531128c05fad1d9873432e168c
ItsGamerik <erschmia@gmail.com>
2025-05-19 13:39:00 +0200

    async handling

--- main.go
+++ main.go
@@ -121,7 +121,8 @@ func main() {
 
 	// Create standard disciplines in the database on startup
 	setup.CreateStandardDisciplines(ctx)
-	setup.CreateStandardRulesets(ctx)
+
+	go setup.CreateStandardRulesets(ctx)
 }
 
 func defineRoutes(ctx context.Context, router *gin.Engine) {
